c7b09ac7d32da0e2922f910d8489f9cb205b9bdb
mrhioui <mohamedrhioui82@gmail.com>
2025-02-22 06:23:00 +0100

    enemies movement, update warship movement

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,22 +1,30 @@
 // here will the game logic and collision algo
 
-const warShip = document.getElementById('game-warship')
-warShip.style.left = '0px'
+import { moveWarShip, shoot } from "./move.js"
+
+const enemies = document.getElementById('game-enemies');
+const frame = document.getElementById('main-frame');
+
+let enemiesStep = 10
+setInterval(() => {
+    let currentLeft = parseInt(enemies.style.left.slice(0, enemies.style.left.length - 2)) || 0;
+    if (currentLeft > frame.offsetWidth - enemies.offsetWidth - 40) {
+        enemiesStep *= -1
+    } else if (currentLeft < 0) {
+        enemiesStep *= -1
+    }
+    enemies.style.left = `${currentLeft + enemiesStep}px`;
+}, 100);
+
 document.addEventListener(
     "keydown",
     (event) => {
         switch (event.key) {
             case 'ArrowRight':
-                console.log('>>>')
-                warShip.style.left = `${parseInt(warShip.style.left.slice(0, warShip.style.left.length-2))+10}px`
-
-                console.log(warShip.style.left)
-                
+                moveWarShip('right')
                 break
             case 'ArrowLeft':
-                if (parseInt(warShip.style.left.slice(0, warShip.style.left.length-2)) >= 10)
-                warShip.style.left = `${parseInt(warShip.style.left.slice(0, warShip.style.left.length-2))-10}px`
-                console.log('<<<')
+                moveWarShip('left')
                 break
             case 'p':
                 console.log('pause')
@@ -27,13 +35,11 @@ document.addEventListener(
             case 'enter':
                 console.log('continue')
                 break
-
             case ' ':
-                console.log('shoot')
+                shoot();
                 break
             default:
                 console.log(event.key)
                 break
         }
-
     })
--- assets/scripts/move.js
+++ assets/scripts/move.js
@@ -1,1 +1,23 @@
 // here will all the game moves ..
+
+export { moveWarShip, shoot }
+
+const frame = document.getElementById('main-frame');
+const warShip = document.getElementById('game-warship');
+warShip.style.left = `${frame.offsetWidth / 2 - 30}px`;
+const bullets = document.getElementById('game-bullets');
+
+const moveWarShip = (move) => {
+    if (move == "right" && (parseInt(warShip.style.left.slice(0, warShip.style.left.length - 2)) < frame.offsetWidth - 2 * warShip.offsetWidth - 10)) {
+        warShip.style.left = `${parseInt(warShip.style.left.slice(0, warShip.style.left.length - 2)) + 10}px`
+    } else if (parseInt(warShip.style.left.slice(0, warShip.style.left.length - 2)) > 0) {
+        warShip.style.left = `${parseInt(warShip.style.left.slice(0, warShip.style.left.length - 2)) - 10}px`
+    }
+}
+
+const shoot = () => {
+    let img = document.createElement('img');
+    img.src = '/assets/imgs/bullet.png';
+    img.style.left = warShip.style.left;
+    frame.appendChild(img)
+}
